e81df27a73341d720f27e7d5c2e42241828ac96f
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2021-01-23 14:41:45 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,4 @@
+def hello():
+	return "hello worl"
+
+hello()	
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+  author="Harun Mbaabu",
+  author_email='mbaabuharun8@gmail.com',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6.8',
+  ],
+  description="Simple python package",
+  license="MIT license",
+  include_package_data=True,
+  name='app',
+  version='0.1.0',
+  zip_safe=False,
+)
